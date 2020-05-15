e3eb39b36c367db4a5b83d059cd79b53959b198d
prabhakantagnihotri <A1s2d3f$>
2020-05-15 14:26:36 +0530

    first commit

--- app/src/main/java/com/example/kotlinadvancetraning/ApplicationClass.kt
+++ app/src/main/java/com/example/kotlinadvancetraning/ApplicationClass.kt
@@ -20,37 +20,7 @@ class ApplicationClass: Application() , HasAndroidInjector{
 
         DaggerAppComponent.builder().application(this)
             .build().inject(this)
-
-//        registerActivityLifecycleCallbacks(object: ActivityLifecycleCallbacks {
-//            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-//                AndroidInjection.inject(activity)
-//            }
-//
-//            override fun onActivityStarted(activity: Activity) {
-//                // Nothing goes here
-//            }
-//
-//            override fun onActivityResumed(activity: Activity) {
-//                // Nothing goes here
-//            }
-//
-//            override fun onActivityPaused(activity: Activity) {
-//                // Nothing goes here
-//            }
-//
-//            override fun onActivityStopped(activity: Activity) {
-//                // Nothing goes here
-//            }
-//
-//            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {
-//                // Nothing goes here
-//            }
-//
-//            override fun onActivityDestroyed(activity: Activity) {
-//                // Nothing goes here
-//            }
-//        })
-
+        
     }
 
     override fun androidInjector(): AndroidInjector<Any> {
